d157abf29a699a5e5fd2ba7fcd6363182e545433
Chris Lloyd-Jones <chris@sealjay.com>
2020-08-21 23:36:48 +0000

    Created Kaiterra data structure, and Pulumi project.

---
+++ src/webapp/Data/KaiterraTelemetry.cs
@@ -0,0 +1,26 @@
+using System;
+
+// representing https://www.kaiterra.com/dev/#devices-all-devices-get
+
+namespace webapp.Data
+{
+
+    public class KaiterraTelemetryPoint {
+        public DateTime ts { get; set; }
+        public float value {get;set}
+    }
+    public class KaiterraTelemetry
+    {
+
+        public string Param {get;set;}
+        public string Units{get;set;}
+        public int Span {get;set;}
+
+        public KaiterraTelemetryPoint[] Points {get;set;}
+    }
+
+    public class KaiterraDeviceReading
+    {
+        public KaiterraTelemetry[] Data {get;set;}
+    }
+}
